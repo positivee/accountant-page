babf27df8d8d8a93f48451de0048f0600cf1d511
positivee <biero.kamil@gmail.com>
2023-05-28 21:48:14 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-=======
   reactStrictMode: true,
->>>>>>> fd604bc18d9a0be8d0d4f4296c583890da0d2465
   compiler: {
     styledComponents: true,
   },
